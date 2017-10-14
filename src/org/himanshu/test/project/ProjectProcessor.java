c974d2e0921333dc7d25891425e7b69f47138b74
Himanshu Gusain <Himanshu Gusain@192.168.1.6>
2017-10-14 23:17:42 +0530

    Removing warnings

--- src/org/himanshu/test/project/ProjectProcessor.java
+++ src/org/himanshu/test/project/ProjectProcessor.java
@@ -3,15 +3,10 @@
  */
 package org.himanshu.test.project;
 
-import java.io.BufferedReader;
 import java.io.BufferedWriter;
 import java.io.File;
-import java.io.FileNotFoundException;
-import java.io.FileReader;
 import java.io.FileWriter;
 import java.io.IOException;
-import java.io.InputStream;
-import java.io.InputStreamReader;
 
 import org.himanshu.test.exception.ProjectDataInvalidException;
 import org.himanshu.test.exception.ProjectFileNotFoundException;
