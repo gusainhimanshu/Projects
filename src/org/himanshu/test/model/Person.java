f35ba1838b91d78a691bf5922d6ff039a6f0aba8
gusainhimanshu <gusainhmanshu@gmail.com>
2018-12-22 02:16:29 +0530

    Duplicate Remove testing code

---
+++ src/org/himanshu/test/model/Person.java
@@ -0,0 +1,56 @@
+package org.himanshu.test.model;
+
+import java.util.Date;
+
+public class Person {
+
+	String Name;
+	Date date;
+	public String getName() {
+		return Name;
+	}
+	public void setName(String name) {
+		Name = name;
+	}
+	public Date getDate() {
+		return date;
+	}
+	public void setDate(Date date) {
+		this.date = date;
+	}
+	@Override
+	public int hashCode() {
+		final int prime = 31;
+		int result = 1;
+		result = prime * result + ((Name == null) ? 0 : Name.hashCode());
+		result = prime * result + ((date == null) ? 0 : date.hashCode());
+		return result;
+	}
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj)
+			return true;
+		if (obj == null)
+			return false;
+		if (getClass() != obj.getClass())
+			return false;
+		Person other = (Person) obj;
+		if (Name == null) {
+			if (other.Name != null)
+				return false;
+		} else if (!Name.equals(other.Name))
+			return false;
+		if (date == null) {
+			if (other.date != null)
+				return false;
+		} else if (!date.equals(other.date))
+			return false;
+		return true;
+	}
+	@Override
+	public String toString() {
+		return "Person [Name=" + Name + ", date=" + date + "]";
+	}
+	
+	
+}
